479c08b353c281eb9e0600f7b96adfdf3e5b5f2d
kevin <kevin@DESKTOP-AT70Q1T>
2024-04-30 22:07:34 -0600

    Dejando tres comentarios en inicio

--- src/Principal/Inicio.java
+++ src/Principal/Inicio.java
@@ -11,6 +11,7 @@ import javax.swing.JFrame;
 
 /**Asi deberia quedar el master ahora si asi**/
 /**Asi deberia quedar el master ahora si asi**/
+/**Asi deberia quedar el master ahora si asi**/
 public class Inicio {
 
     public static void main(String[] args) {
